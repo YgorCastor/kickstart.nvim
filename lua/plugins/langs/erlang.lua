ebba26aeb35c81b2005e07d61b45296f7235d843
Ygor Castor <ygorcastor@gmail.com>
2025-05-05 10:21:12 +0200

    Added some more langs

---
+++ lua/plugins/langs/erlang.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        erlangls = {},
+      },
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = { ensure_installed = { 'erlang' } },
+  },
+}
---
+++ lua/plugins/langs/gleam.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = { ensure_installed = { 'gleam' } },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        gleam = {},
+      },
+    },
+  },
+}
